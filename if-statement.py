2b31eddfd9d52aef334f4779dd8c73a45a103afa
hatalabdallah@gmail.com <hatalabdallah@gmail.com>
2022-08-20 14:18:38 +0300

    mmm

---
+++ if-statement.py
@@ -0,0 +1,42 @@
+# a = 10
+# b = 6
+# c = 6
+
+# if a>b and b == c: 
+#     print("condition is true")
+    
+# elif b == c:
+#     print("condition is true")
+
+# elif b == c:
+#     print("condition is true")
+
+# elif b == c:
+#     print("condition is true")
+
+# elif b == c:
+#     print("condition is true")
+    
+# else:
+#     print("Please try again")
+
+#BITWISE OPERATORS
+
+# & bitwise and this returns 1 if both bits are 1 or 0.
+
+
+# | bitwise oR  this returns 1 if either of the bits is 1 or 0.
+
+
+#how to use if on strings
+
+price = 50
+
+if price > 100:
+    print("Price is greater")
+
+elif price == 100:
+    print("Price is 100")
+    
+else:
+    print("price is less")
